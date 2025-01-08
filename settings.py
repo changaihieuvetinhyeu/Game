0bacfc1240fa285d0351da9b9b1e289d4c77d956
changaihieuvetinhyeu <thanbai585@gmail.com>
2025-01-08 16:57:51 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -30,29 +30,3 @@ WORLD_LAYERS = {
 	'main': 3,
 	'top': 4
 }
-
-BATTLE_POSITIONS = {
-	'left': {'top': (360, 260), 'center': (190, 400), 'bottom': (410, 520)},
-	'right': {'top': (900, 260), 'center': (1110, 390), 'bottom': (900, 550)}
-}
-
-BATTLE_LAYERS =  {
-	'outline': 0,
-	'name': 1,
-	'monster': 2,
-	'effects': 3,
-	'overlay': 4
-}
-
-BATTLE_CHOICES = {
-	'full': {
-		'fight':  {'pos' : vector(30, -60), 'icon': 'sword'},
-		'defend': {'pos' : vector(40, -20), 'icon': 'shield'},
-		'switch': {'pos' : vector(40, 20), 'icon': 'arrows'},
-		'catch':  {'pos' : vector(30, 60), 'icon': 'hand'}},
-	
-	'limited': {
-		'fight':  {'pos' : vector(30, -40), 'icon': 'sword'},
-		'defend': {'pos' : vector(40, 0), 'icon': 'shield'},
-		'switch': {'pos' : vector(30, 40), 'icon': 'arrows'}}
-}
