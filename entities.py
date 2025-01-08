8cf863ac98cb0276d3bba4e8495238766db60812
changaihieuvetinhyeu <thanbai585@gmail.com>
2025-01-08 16:36:04 +0700

    Add files via upload
    File nền của Game

---
+++ entities.py
@@ -0,0 +1,115 @@
+from settings import *
+
+class Entity(pygame.sprite.Sprite):
+    def __init__(self,pos,frames,groups,facing_direction):
+        super().__init__(groups)
+        self.z = WORLD_LAYERS['main']
+        
+        self.frame_index,self.frames = 0, frames
+        self.facing_direction = facing_direction
+        self.direction = vector()
+        self.speed = 250
+        self.blocked = False
+        self.image = self.frames[self.get_state()][self.frame_index] 
+        self.rect = self.image.get_rect(center = pos)
+        self.hitbox = self.rect.inflate(-self.rect.width/2,-60)
+
+        self.y_sort = self.rect.centery
+        
+
+    def animate(self,dt):
+        self.frame_index += ANIMATION_SPEED * dt
+        self.image = self.frames[self.get_state()][int(self.frame_index % len(self.frames[self.get_state()]))]
+
+    def get_state(self):
+        moving = bool(self.direction)
+        if moving:
+            if self.direction.x != 0:
+                self.facing_direction = 'right' if self.direction.x > 0 else 'left'
+            if self.direction.y != 0:
+                self.facing_direction = 'down' if self.direction.y > 0 else 'up'
+        return f'{self.facing_direction}{'' if moving else '_idle'}'
+
+    def opposite_direction(self):
+        if self.facing_direction == 'up':
+            self.facing_direction = 'down'
+        elif self.facing_direction == 'down':
+            self.facing_direction = 'up'
+        elif self.facing_direction == 'left':
+            self.facing_direction = 'right'
+        elif self.facing_direction == 'right':
+            self.facing_direction = 'left'
+
+
+    def change_facing_direction(self,target_pos):
+        relation = vector(target_pos) - vector(self.rect.center)
+        if abs(relation.y) < 30:
+            self.facing_direction = 'right' if relation.x > 0 else 'left'
+        else:
+            self.facing_direction = 'down' if relation.y > 0 else 'up'
+
+    def block(self):
+        self.blocked = True
+        self.direction = vector(0,0)
+
+    def unblock(self):
+        self.blocked = False
+
+class Character(Entity):
+    def __init__(self,pos,frames,groups,facing_direction):
+        super().__init__(pos,frames,groups,facing_direction)
+        
+    def update(self,dt):
+        self.animate(dt)
+
+class Player(Entity):
+    def __init__(self,pos,frames,groups,facing_direction,collision_sprites):
+        super().__init__(pos,frames,groups,facing_direction)
+        self.collision_sprites = collision_sprites
+        
+    def input(self):
+        keys = pygame.key.get_pressed()
+        input_vector = vector()
+        if keys[pygame.K_UP]:
+            input_vector.y -= 1
+        if keys[pygame.K_DOWN]:
+            input_vector.y += 1
+        if keys[pygame.K_LEFT]:
+            input_vector.x -= 1
+        if keys[pygame.K_RIGHT]:
+            input_vector.x += 1
+        self.direction = input_vector.normalize() if input_vector else input_vector
+
+    def move(self,dt):
+        self.rect.centerx += self.direction.x * self.speed * dt
+        self.hitbox.centerx = self.rect.centerx
+        self.collision('horizontal')
+
+        self.rect.centery += self.direction.y * self.speed * dt
+        self.hitbox.centery = self.rect.centery
+        self.collision('vertical')
+
+
+    def collision(self, axis):
+        for sprite in self.collision_sprites:
+            if sprite.hitbox.colliderect(self.hitbox):
+                if axis == 'horizontal':
+                    if self.direction.x > 0:
+                        self.hitbox.right = sprite.hitbox.left
+                    if self.direction.x < 0:
+                        self.hitbox.left = sprite.hitbox.right
+                    self.rect.centerx = self.hitbox.centerx
+                else:
+                    if self.direction.y > 0:
+                        self.hitbox.bottom = sprite.hitbox.top
+                    if self.direction.y < 0:
+                        self.hitbox.top = sprite.hitbox.bottom
+                    self.rect.centery = self.hitbox.centery
+
+
+    def update(self,dt):
+        self.y_sort = self.rect.centery
+        if not self.blocked:
+            self.input()
+            self.move(dt)
+        self.animate(dt)
---
+++ main.py
@@ -0,0 +1,199 @@
+from settings import *
+from game_data import *
+from pytmx.util_pygame import load_pygame
+from sprites import Sprite,AnimatedSprite,BorderSprite,CollidableSprite,TransitionSprite
+from entities import Player,Character
+from groups import AllSprites
+from support import *
+from os.path import join
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+        self.clock = pygame.time.Clock()
+
+
+        self.all_sprites = AllSprites()
+        self.collision_sprites = pygame.sprite.Group()
+        self.character_sprites = pygame.sprite.Group()
+        self.transition_sprites = pygame.sprite.Group()
+
+        self.transition_target = None
+        self.tint_surf = pygame.Surface((WINDOW_WIDTH,WINDOW_HEIGHT))
+        self.tint_mode = 'untint'
+        self.tint_progress = 0
+        self.tint_direction = -1
+        self.tint_speed = 600
+
+        self.import_assests()
+        self.setup(self.tmx_maps['world'],'house')
+        
+    def import_assests(self):
+        self.tmx_maps = tmx_importer('data','maps')
+
+        self.overworld_frames = {
+            'water':import_folder('graphics','tilesets','water'),
+            'coast': coast_importer(24,12,'graphics','tilesets','coast'),
+            'characters': all_character_import('graphics','characters')
+            }
+        
+        self.fonts = {
+            'dialog': pygame.font.Font(join('graphics','fonts','PixeloidSans.ttf'),30),
+            'intro': pygame.font.Font(join('graphics','fonts','PixeloidSans.ttf'),50)
+        }
+
+    def intro_screen(self):
+        running = True
+        font = self.fonts['intro']
+        while running:
+            self.display_surface.fill(COLORS['black'])  
+
+            
+            title_text = font.render("Test", True, (255, 255, 255))
+            title_rect = title_text.get_rect(center=(WINDOW_WIDTH // 2, 150))
+            self.display_surface.blit(title_text, title_rect)
+
+            
+            mouse_pos = pygame.mouse.get_pos()
+            start_button = pygame.Rect(WINDOW_WIDTH // 2 - 100, 300, 200, 60)
+            quit_button = pygame.Rect(WINDOW_WIDTH // 2 - 100, 400, 200, 60)
+
+            # Kiểm tra con chuột
+            pygame.draw.rect(self.display_surface, (0, 102, 204) if start_button.collidepoint(mouse_pos) else (150, 150, 150), start_button)
+            pygame.draw.rect(self.display_surface, (0, 102, 204) if quit_button.collidepoint(mouse_pos) else (150, 150, 150), quit_button)
+
+            # Hiển thị text của nút
+            start_text = font.render("Start", True, (255, 255, 255))
+            quit_text = font.render("Quit", True, (255, 255, 255))
+            self.display_surface.blit(start_text, start_text.get_rect(center=start_button.center))
+            self.display_surface.blit(quit_text, quit_text.get_rect(center=quit_button.center))
+
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if start_button.collidepoint(event.pos):
+                        running = False  
+                    elif quit_button.collidepoint(event.pos):
+                        pygame.quit()
+                        exit()
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+    def setup(self,tmx_map,player_start_pos):
+        for group in (self.all_sprites,self.collision_sprites,self.transition_sprites,self.character_sprites):
+            group.empty()
+
+        for layer in ['Terrain','Terrain Top']:
+            for x,y,surf in tmx_map.get_layer_by_name(layer).tiles():
+                Sprite((x * TILE_SIZE, y * TILE_SIZE),surf,self.all_sprites,WORLD_LAYERS['bg'])
+        
+        for obj in tmx_map.get_layer_by_name('Water'):
+            for x in range(int(obj.x),int(obj.x + obj.width),TILE_SIZE):
+                for y in range(int(obj.y),int(obj.y + obj.height), TILE_SIZE):
+                    AnimatedSprite((x,y),self.overworld_frames['water'],self.all_sprites,WORLD_LAYERS['water'])
+
+        for obj in tmx_map.get_layer_by_name('Coast'):
+            terrain = obj.properties['terrain']
+            side = obj.properties['side']
+            AnimatedSprite((obj.x,obj.y),self.overworld_frames['coast'][terrain][side],self.all_sprites,WORLD_LAYERS['bg'])
+
+        for obj in tmx_map.get_layer_by_name('Objects'):
+            if obj.name == 'top':
+                Sprite((obj.x,obj.y),obj.image,self.all_sprites,WORLD_LAYERS['top'])
+            else:
+                CollidableSprite((obj.x,obj.y),obj.image,(self.all_sprites,self.collision_sprites))
+
+        for obj in tmx_map.get_layer_by_name('Transition'):
+            TransitionSprite((obj.x,obj.y),(obj.width,obj.height),(obj.properties['target'],obj.properties['pos']),self.transition_sprites)
+
+        for obj in tmx_map.get_layer_by_name('Collisions'):
+            BorderSprite((obj.x,obj.y),pygame.Surface((obj.width,obj.height)),self.collision_sprites)
+
+        for obj in tmx_map.get_layer_by_name('Entities'):
+            if obj.name == 'Player' and obj.properties['pos'] == player_start_pos:
+                self.player = Player(
+                    pos = (obj.x,obj.y),
+                    frames = self.overworld_frames['characters']['player'],
+                    groups = self.all_sprites,
+                    facing_direction = obj.properties['direction'],
+                    collision_sprites = self.collision_sprites)
+            elif obj.name == 'Character': 
+                Character(
+                    pos = (obj.x,obj.y),
+                    frames = self.overworld_frames['characters'][obj.properties['graphic']],
+                    groups = (self.all_sprites,self.collision_sprites,self.character_sprites),
+                    facing_direction = obj.properties['direction'],
+                )
+        
+    def input(self):
+        key = pygame.key.get_pressed()
+        if key[pygame.K_SPACE]:
+            for character in self.character_sprites:
+                if check_connection(100,self.player,character):
+                    self.player.block()
+                    character.change_facing_direction(self.player.rect.center)
+                    self.player.unblock()
+
+    def transition_box(self):
+        font = self.fonts['dialog']
+        message = "Do you want to enter this area? (Y)"
+        text = font.render(message, True, (255, 255, 255))
+        rect = text.get_rect(center=(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2))
+        pygame.draw.rect(self.display_surface, (0, 0, 0), rect.inflate(20, 20))
+        self.display_surface.blit(text, rect)
+
+    def transition_check(self):
+        sprites = [sprite for sprite in self.transition_sprites if sprite.rect.colliderect(self.player.hitbox)]
+        key = pygame.key.get_pressed()
+        if sprites:
+            self.player.block()
+            self.transition_box()
+            if key[pygame.K_y]:
+                self.transition_target = sprites[0].target
+                self.tint_mode = 'tint'
+            elif key[pygame.K_UP] or key[pygame.K_DOWN] or key[pygame.K_LEFT] or key[pygame.K_RIGHT] :
+                self.player.unblock()
+
+    def tint_screen(self,dt):
+        if self.tint_mode == 'untint':
+            self.tint_progress -= self.tint_speed * dt
+        
+        if self.tint_mode == 'tint':
+            self.tint_progress += self.tint_speed * dt
+            if self.tint_progress >= 255:
+                self.setup(self.tmx_maps[self.transition_target[0]],self.transition_target[1])
+                self.tint_mode = 'untint'
+                self.transition_target = None
+
+        self.tint_progress = max(0, min(self.tint_progress, 255))
+        self.tint_surf.set_alpha(self.tint_progress)
+        self.display_surface.blit(self.tint_surf,(0,0))
+    
+    def run(self):
+        self.intro_screen()
+
+        while True:
+            dt = self.clock.tick() / 1000
+            self.display_surface.fill('black')
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+            #Cập nhật
+            self.input()
+            self.transition_check()
+            self.all_sprites.update(dt)
+            self.all_sprites.draw(self.player.rect.center)
+            
+            self.tint_screen(dt)
+            pygame.display.update()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
